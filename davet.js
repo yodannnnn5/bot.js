26f1390dba4225526831827d1e92ca37c0fea7b5
yodannnnn5 <111691767+yodannnnn5@users.noreply.github.com>
2022-10-19 20:24:38 +0300

    Add files via upload

---
+++ davet.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+const db = require("quick.db");
+const ayarlar = require("../ayarlar/bot.json");
+
+exports.run = async (client, message, args) => {
+  	let p = db.fetch(`prefix.${message.guild.id}`) || ayarlar.prefix;
+
+const embed = new Discord.MessageEmbed()
+.setColor("#ffffff")
+.setAuthor(`cod3j Linkler`, client.user.avatarURL())
+.setDescription(`[cod3j botun yeri.](https://discord.gg/AvKrzpthK2`)
+.setFooter(`${message.author.username} tarafından istendi!`) 
+.setTimestamp()
+.setThumbnail(client.user.avatarURL())
+message.channel.send(embed)   
+ };
+
+ exports.config = {
+      name: 'davet',
+   aliases: ["invitation","site"]
+ };
